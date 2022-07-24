74bd492a6770f4cdd2ec011892e08bf33df6ac97
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2022-07-24 18:32:23 +0100

    .

--- proj_excel_comp/my_app/views.py
+++ proj_excel_comp/my_app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+import openpyxl
+# openpyxl module to read Excel file in Django.
 
 # Create your views here.
+#This view will be responsible to read the excel file
+
+def index(request):
+    if "GET" == request.method:
+        return render(request, 'my_app/index.html', {})
+    else:
+        excel_file = request.FILES["excel_file"]
+
+        # you may put validations here to check extension or file size
+
+        wb = openpyxl.load_workbook(excel_file)
+
+        # getting a particular sheet by name out of many sheets
+        worksheet = wb["Sheet1"]
+        print(worksheet)
+
+        # getting active sheet
+        active_sheet = wb.active
+        print(active_sheet)
+
+        excel_data = list()
+        # iterating over the rows and
+        # getting value from each cell in row
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+
+        return render(request, 'myapp/index.html', {"excel_data":excel_data})
--- proj_excel_comp/proj_excel_comp/settings.py
+++ proj_excel_comp/proj_excel_comp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_app',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'proj_excel_comp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
